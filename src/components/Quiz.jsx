18d877453f61dcc822bfd8a574b8660c24faee1f
vibhorsingh11 <vibhorsinghcs@gmail.com>
2024-04-10 14:59:18 +0530

    added summary result screen for the quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useCallback, useState } from 'react';
 
 import QUESTIONS from '../questions.js';
-import quizCompleteImg from '../assets/quiz-complete.png';
 import Question from './Question.jsx';
+import Summary from './Summary.jsx';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -27,12 +27,7 @@ export default function Quiz() {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   //When the key={} value changes it will recreate that component
